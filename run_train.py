53b8c59afd1cc18a0d1abd1a24440705f9f01a5c
Pavel Bezglasny <pavelb23@gmail.com>
2022-01-26 20:30:21 +0300

    Update learn results.

--- run_train.py
+++ run_train.py
@@ -3,7 +3,11 @@
 from subprocess import Popen, PIPE, CalledProcessError
 
 # 'gpt2'
-models = ['bert-base-uncased', 'distilbert-base-uncased', 'bert-base-multilingual-cased', 'bert-base-cased']
+models = [
+    'bert-base-uncased',
+    'distilbert-base-uncased',
+    # 'bert-base-multilingual-cased',
+    'bert-base-cased']
 # models = ['bert-base-uncased']
 percents = '5,10,15,20'
 # percents = '5'
